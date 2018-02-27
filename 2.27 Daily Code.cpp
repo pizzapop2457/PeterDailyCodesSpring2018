977902bc4caa85dd4027972d78801b6a730afa6d
Peter Pham <31864835+pizzapop2457@users.noreply.github.com>
2018-02-27 13:06:09 -0700

    Add files via upload

---
+++ 2.27 Daily Code.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main() {
+	string line;
+	ifstream myfile("Prob01.in.txt");
+	if (myfile.is_open())
+	{
+		while (getline(myfile, line))
+		{
+			cout << line << endl;
+			cout << line << endl;
+		}
+		myfile.close();
+	}
+	else
+		cout << "Unable to open file";
+	system("pause");
+}
