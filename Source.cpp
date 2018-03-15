321c20a1bf605a82a80b2102385677492662ee18
Peter Pham <31864835+pizzapop2457@users.noreply.github.com>
2018-03-15 12:32:42 -0600

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int input = 64;
+	vector <int> numbers;
+	vector <int> ::iterator i;
+
+
+	//get unput from users until they press 0
+	while (input != 0) {
+		cin >> input;
+		numbers.push_back(input);
+	}
+
+
+	//sort it!
+	sort(numbers.begin(), numbers.end());
+
+
+	//print it back out!
+	for (i = numbers.begin(); i < numbers.end(); i++) {
+		cout << "the numbers in order are:";
+		cout << *i << endl;
+	}
+	system("pause");
+
+}
